d0518d7221aa1dfd20f69a75d0a56da042e03f91
Diego <bdu.diego12@gmail.com>
2011-02-07 15:16:09 -0200

    +Adicionei classe q gera pdf +Adicionei classe command para gerar estatistica, mas falta inserir dados do bd no grafico, fabrica de comandos e falta testar ~Mudei o dataset para adicionar valores no grafico, sem a necessidade de outras classes ~mudei o testegrafico para testar o pdf, salva no diretorio c:\arquiv\, basta mudar na classe testegrafico
    referencia:
    http://www.geek-tutorials.com/java/itext/servlet_jsp_output_pdf.php

--- ProjetoEngyos/src/com/epucjr/engyos/teste/TesteGrafico.java
+++ ProjetoEngyos/src/com/epucjr/engyos/teste/TesteGrafico.java
@@ -2,6 +2,10 @@ package com.epucjr.engyos.teste;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -11,6 +15,7 @@ import com.epucjr.engyos.dominio.modelo.Obreiro;
 import com.epucjr.engyos.dominio.modelo.Reuniao;
 import com.epucjr.engyos.tecnologia.utilitarios.DataSet;
 import com.epucjr.engyos.tecnologia.utilitarios.Grafico;
+import com.epucjr.engyos.tecnologia.utilitarios.GeradorPDF;
 
 public class TesteGrafico {
 
@@ -30,8 +35,26 @@ public class TesteGrafico {
 	public void initComponentsTest() {
 		//Gera os graficos
 		Color c = Color.getHSBColor((float)Math.random(), 1f,1f);//gera cor aleatorio
-		label0 = new JLabel(new ImageIcon(gerarGrafico3D(0, c)));
-		label1 = new JLabel(new ImageIcon(gerarGrafico3D(1, null)));
+		
+		BufferedImage bi0 = gerarGrafico3D(0, c);
+		BufferedImage bi1 = gerarGrafico3D(1, null);
+		label0 = new JLabel(new ImageIcon(bi0));
+		label1 = new JLabel(new ImageIcon(bi1));
+		
+		try {
+			FileOutputStream fos0 = new FileOutputStream(new File("c:\\arquiv\\test0.pdf"));
+			FileOutputStream fos1 = new FileOutputStream(new File("c:\\arquiv\\test1.pdf"));
+			GeradorPDF.gerarPDF(bi0, fos0);
+			GeradorPDF.gerarPDF(bi1, fos1);
+			fos0.close();
+			fos1.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 		//Instancia as janelas
 		tela0 = new JFrame("modo 0");
@@ -58,9 +81,9 @@ public class TesteGrafico {
 	 * Adiciona reuniao no DataSet
 	 * @param r
 	 */
-	public void addReuniao(Reuniao r) {
-		dataset0.addReuniao(r);
-		dataset1.addReuniao(r);
+	public void addReuniao(int mes, Number valor) {
+		dataset0.addValor(mes, valor);
+		dataset1.addValor(mes, valor);
 	}
 	
 	/**
@@ -80,20 +103,12 @@ public class TesteGrafico {
 		TesteGrafico tg = new TesteGrafico();
 		
 		//add as reunioes
-		Reuniao r;
-		String mes;
-		for (int k = 1; k <= 12; k++) {
-			
-			mes = "" + k;
-			if (mes.length() == 1) mes = "0" + mes;
-			r = new Reuniao("14" + mes + "2010","5:40");
-			
-			for (float i = 0; i < Math.random(); i = i + 0.02f) r.addObreiroListaDePresenca(new Obreiro());
-			
-			tg.addReuniao(r);
+		for (int k = 0; k < 12; k++) {		
+			tg.addReuniao(k, (int)(Math.random()*20));
 		}
 		
 		tg.initComponentsTest();
 		
 	}
+
 }
