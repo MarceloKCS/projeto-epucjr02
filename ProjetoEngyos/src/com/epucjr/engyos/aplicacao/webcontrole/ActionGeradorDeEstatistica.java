ab2cee4d7a77c1f6a9a0e77a5df1cfe8562d870e
Diego <bdu.diego12@gmail.com>
2011-02-09 12:50:13 -0200

    +adicionei pdfs ~alterei a classe geradora de estatistica, deve gerar um grafico q contabiliza a presenca dos obreiros no ano apartir de reunioes selecionadas

--- ProjetoEngyos/src/com/epucjr/engyos/aplicacao/webcontrole/ActionGeradorDeEstatistica.java
+++ ProjetoEngyos/src/com/epucjr/engyos/aplicacao/webcontrole/ActionGeradorDeEstatistica.java
@@ -3,16 +3,24 @@ package com.epucjr.engyos.aplicacao.webcontrole;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.epucjr.engyos.aplicacao.controle.Command;
+import com.epucjr.engyos.dominio.modelo.Reuniao;
+import com.epucjr.engyos.tecnologia.persistencia.DataAccessObjectManager;
 import com.epucjr.engyos.tecnologia.utilitarios.DataSet;
 import com.epucjr.engyos.tecnologia.utilitarios.GeradorPDF;
 import com.epucjr.engyos.tecnologia.utilitarios.Grafico;
 
 public class ActionGeradorDeEstatistica implements Command {
 
+	/**
+	 * 0-request
+	 * 1-response
+	 */
 	public Object execute(Object... arg) {
 		// TODO Auto-generated method stub
 		
@@ -20,6 +28,7 @@ public class ActionGeradorDeEstatistica implements Command {
 		int modo = 0;
 		Color cor = Color.RED;
 		HttpServletResponse response = (HttpServletResponse) arg[1];
+		HttpServletRequest request = (HttpServletRequest) arg[0];
 		
 		//mostra pdf no browser, n sei se isso deve estar antes ou depois
 		response.setContentType("application/pdf");
@@ -27,16 +36,27 @@ public class ActionGeradorDeEstatistica implements Command {
 			 " attachment; filename=\"Estatistica.pdf\"");//usado para salvar
 		
 		//pega os dados do bd
+		DataAccessObjectManager dao = new DataAccessObjectManager();
+		String ano = request.getAttribute("Ano").toString();//Ano
+		List<Long> listaReuniao = (List<Long>) request.getAttribute("ListaIdReuniao");//lista de reunioes seleecionada
+		
 		
 		//cria o dataset
 		DataSet dataSet = new DataSet(modo);
 		
 		//insere os dados do bd no dataset
+		for (long id: listaReuniao) {
+			Reuniao r = dao.obterReuniao(id); //obtem a reuniao
+			if (r.getAno().equals(ano)) {
+				int i = Integer.parseInt(r.getMes()) - 1;
+				dataSet.addValor(i, r.getListaDePresencaObreiro().size());
+			}
+		}
 		
 		//cria o grafico
 		BufferedImage bi = null;
-		if (modo == 0) bi = Grafico.gerarGrafico3D(null, "Presenca 2010", "Mês", "Presença", 800, 500, 800, 500, dataSet.getDcd(), 0.75f, cor, modo);
-		if (modo == 1) bi =  Grafico.gerarGrafico3D(null, "Presenca 2010", "Mês", "Presença", 800, 500, 800, 500, dataSet.getDcd(), 0.75f, null, modo);
+		if (modo == 0) bi = Grafico.gerarGrafico3D(null, "Presença "+ano, "Mês", "Presença", 800, 500, 800, 500, dataSet.getDcd(), 0.75f, cor, modo);
+		if (modo == 1) bi = Grafico.gerarGrafico3D(null, "Presença "+ano, "Mês", "Presença", 800, 500, 800, 500, dataSet.getDcd(), 0.75f, null, modo);
 		
 		//gera o pdf
 		try {
